06710661f05100f5df3a1592eaf7a6279f1db17b
Gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2021-07-05 09:01:14 -0400

    Initial commit

---
+++ simple_check.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# https://www.tecmint.com/find-linux-processes-memory-ram-cpu-usage/
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head
+
+# Files with open connections on the network
+
+lsof -i
